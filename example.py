ea6519961ea73e175b2c9aa3b5b9744c2b387e9e
2323-code <f_shibata@MacBook-Air.local>
2024-07-28 19:49:18 +0900

    fix: stringsを使うやり方を試してみる

--- example.py
+++ example.py
@@ -1,7 +1,17 @@
-# https://numpy.org/doc/stable/user/quickstart.html
+# https://qiita.com/mo256man/items/3578f374cd775ce54cb8
+import datetime
+
 import numpy as np
 
-print(np.__version__)
+print("ver", np.__version__)
+# 2.0.1
 
-a = np.arange(15).reshape(3, 5)
-print(a)
+
+now1 = datetime.datetime.now()
+l = np.strings.add(["num", "doc"], ["py", "umentation"])
+# l = np.char.add(["num", "doc"], ["py", "umentation"]) # ver 1.x まで
+print(l.dtype)
+print(l)
+now2 = datetime.datetime.now()
+
+print(now2 - now1)
